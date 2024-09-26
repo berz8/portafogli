8b42f5f79a42fb313a7302117218bf26f085cc03
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-26 20:18:15 +0200

    fixed homepage height

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Wallet } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 items-center justify-center">
+    <div className="flex flex-col gap-8 items-center justify-center min-h-screen">
+      <Wallet className="w-24 h-24 text-stone-700" />
       <h1 className="font-extrabold text-3xl text-center px-6">
         The App to track all your expenses
       </h1>
