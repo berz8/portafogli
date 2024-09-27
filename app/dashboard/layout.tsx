a7761a9cdf77bef58e086761ede3983b09fc3fd9
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-27 23:20:39 +0200

    moved navigation to a component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,5 @@
 import { auth } from "@/auth";
-import { cn } from "@/lib/utils";
-import { CirclePlus, Home, Settings } from "lucide-react";
-import Link from "next/link";
+import Navigation from "@/components/navigation";
 import { redirect } from "next/navigation";
 
 export default async function DashboadLayout({
@@ -17,22 +15,7 @@ export default async function DashboadLayout({
 
   return (
     <div className="relative">
-      <div
-        className={cn(
-          "fixed flex justify-around items-center bottom-0 left-0 w-full p-2 shadow-lg bg-stone-50",
-          "md:w-auto md:left-1/2 md:-translate-x-1/2 md:bottom-4 md:rounded-lg md:px-4 md:py-2",
-        )}
-      >
-        <Link href="/dashboard" className="px-4 block py-2">
-          <Home className="w-6 h-6" />
-        </Link>
-        <Link href="/dashboard/new-expense" className="px-4 block py-2">
-          <CirclePlus className="w-6 h-6" />
-        </Link>
-        <Link href="/dashboard/settings" className="px-4 block py-2">
-          <Settings className="w-6 h-6" />
-        </Link>
-      </div>
+      <Navigation />
       <div className="text-center mb-2">
         <h1 className="font-black py-1">PORTAFOGLI</h1>
       </div>
---
+++ components/navigation.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Link } from "next-view-transitions";
+import { usePathname } from "next/navigation";
+import { CirclePlus, Home, Settings } from "lucide-react";
+
+export default function Navigation() {
+  const currentPath = usePathname();
+
+  return (
+    <div
+      className={cn(
+        "fixed flex justify-around items-center bottom-2 left-2 w-[calc(100%-16px)] rounded-lg p-2 shadow-lg bg-stone-50",
+        "md:w-auto md:left-1/2 md:-translate-x-1/2 md:bottom-4 md:rounded-lg md:px-4 md:py-2",
+      )}
+    >
+      <Link
+        href="/dashboard"
+        className={cn(
+          "px-4 block py-1",
+          currentPath === "/dashboard" && "text-green-700",
+        )}
+      >
+        <Home className="w-5 h-5" />
+      </Link>
+      <Link
+        href="/dashboard/new-expense"
+        className={cn(
+          "px-4 block py-1",
+          currentPath === "/new-expense" && "text-green-700",
+        )}
+      >
+        <CirclePlus className="w-5 h-5" />
+      </Link>
+      <Link
+        href="/dashboard/settings"
+        className={cn(
+          "px-4 block py-1",
+          currentPath === "/dashboard/settings" && "text-green-700",
+        )}
+      >
+        <Settings className="w-5 h-5" />
+      </Link>
+    </div>
+  );
+}
