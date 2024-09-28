49a59955cc05e8c5db8c3a586ec09b41aa40f3a8
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-28 11:19:18 +0200

    feat: pwa support

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Portafogli",
+    short_name: "Portafogli",
+    description: "The App to track all your expenses",
+    display: "standalone",
+    background_color: "#ffffff",
+    scope: "https://portafogli.berz.it/",
+    id: "https://portafogli.berz.it/?source=pwa",
+    start_url: "https://portafogli.berz.it/?source=pwa",
+    icons: [
+      {
+        src: "/icon-192-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
