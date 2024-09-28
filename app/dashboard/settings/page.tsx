4dab83949d4869978b00b863f051b4bc3cbd5c52
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-28 12:21:10 +0200

    logout button

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
 import { users } from "@/db/schema/users";
 import { eq } from "drizzle-orm";
+import { signOut } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Settings() {
@@ -15,9 +17,11 @@ export default async function Settings() {
   });
 
   return (
-    <div className="px-4">
-      <div className="rounded-md shadow-lg bg-stone-100 p-2 pb-3 px-3">
-        <h2 className="text-lg text-center font-bold pb-4">Settings</h2>
+    <div>
+      <div className="rounded-xl shadow-lg bg-stone-100 p-4 mb-6">
+        <h2 className="text-lg text-center font-bold pb-4 font-mono">
+          Settings
+        </h2>
         <div className="flex gap-4 items-center">
           <div className="rounded-full w-14 h-14 overflow-hidden">
             <img src={userData?.image ?? undefined} alt="profile pic" />
@@ -28,6 +32,19 @@ export default async function Settings() {
           </div>
         </div>
       </div>
+      <form
+        className="mt-20"
+        action={async () => {
+          "use server";
+          await signOut({
+            redirectTo: "/",
+          });
+        }}
+      >
+        <Button variant="secondary" className="w-full" type="submit">
+          Sign Out
+        </Button>
+      </form>
     </div>
   );
 }
