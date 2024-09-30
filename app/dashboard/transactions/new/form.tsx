0bc59fe7f92a2f19dfba87226c8808593d2342a8
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-30 09:41:14 +0200

    segmented conntrols style

--- app/dashboard/transactions/new/form.tsx
+++ app/dashboard/transactions/new/form.tsx
@@ -72,7 +72,7 @@ export default function FormExpense({ userId }: { userId: string }) {
                 <RadioGroup
                   onValueChange={field.onChange}
                   defaultValue={field.value}
-                  className="flex bg-stone-200 shadow-inner p-1 rounded-lg w-full"
+                  className="flex bg-stone-200 shadow-[inset_0_0_4px_2px_rgba(0,0,0,0.1)] p-1 rounded-lg w-full"
                 >
                   <FormItem className="flex items-center grow space-y-0">
                     <FormControl>
@@ -187,23 +187,25 @@ export default function FormExpense({ userId }: { userId: string }) {
             </FormItem>
           )}
         />
-        <div className="fixed bottom-[CALC(2.9rem_+_env(safe-area-inset-bottom))] left-0 px-4 w-full">
-          <div className="relative">
-            <svg
-              className="w-full h-4 text-[#F6F6F6]"
-              viewBox="0 0 100 10"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M100 0C96.6667 6.66667 93.3333 6.66667 90 -8.74228e-07C86.6667 6.66667 83.3333 6.66667 80 -1.74846e-06C76.6667 6.66666 73.3333 6.66666 70 -2.62268e-06C66.6667 6.66666 63.3333 6.66666 60 -3.49691e-06C56.6667 6.66666 53.3333 6.66666 50 -4.37114e-06C46.6667 6.66666 43.3333 6.66666 40 -5.24537e-06C36.6667 6.66666 33.3333 6.66666 30 -6.11959e-06C26.6667 6.66666 23.3333 6.66666 20 -6.99382e-06C16.6667 6.66666 13.3333 6.66666 10 -7.86805e-06C6.66667 6.66666 3.33333 6.66666 8.74228e-07 -8.74228e-06L0 9.99999L100 10L100 0Z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
-          <div className="w-full bg-gradient-to-b from-[#F6F6F6] to-primary-foreground px-3 pt-4 pb-4">
-            <Button className="w-full" type="submit">
-              Add
-            </Button>
+        <div className="fixed bottom-[CALC(2.9rem_+_env(safe-area-inset-bottom))] px-4 left-0 w-full">
+          <div className="drop-shadow-[0_-8px_3px_rgba(150,150,150,0.05)] px-4 -mx-4">
+            <div className="relative">
+              <svg
+                className="w-full h-4 text-[#F6F6F6]"
+                viewBox="0 0 100 10"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M100 0C96.6667 6.66667 93.3333 6.66667 90 -8.74228e-07C86.6667 6.66667 83.3333 6.66667 80 -1.74846e-06C76.6667 6.66666 73.3333 6.66666 70 -2.62268e-06C66.6667 6.66666 63.3333 6.66666 60 -3.49691e-06C56.6667 6.66666 53.3333 6.66666 50 -4.37114e-06C46.6667 6.66666 43.3333 6.66666 40 -5.24537e-06C36.6667 6.66666 33.3333 6.66666 30 -6.11959e-06C26.6667 6.66666 23.3333 6.66666 20 -6.99382e-06C16.6667 6.66666 13.3333 6.66666 10 -7.86805e-06C6.66667 6.66666 3.33333 6.66666 8.74228e-07 -8.74228e-06L0 9.99999L100 10L100 0Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <div className="w-full bg-gradient-to-b from-[#F6F6F6] to-primary-foreground px-3 pt-4 pb-4">
+              <Button className="w-full" type="submit">
+                Add
+              </Button>
+            </div>
           </div>
         </div>
       </form>
