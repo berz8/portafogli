03eb49e4d80defb54a7bca1d7bc66b4dae83ba76
Davide Lombardi <davidelombardi130p@gmail.com>
2024-10-10 19:50:56 +0200

    fix(categories-all): channged to action

--- app/dashboard/categories/all/page.tsx
+++ app/dashboard/categories/all/page.tsx
@@ -1,21 +1,11 @@
+import { getCategories } from "@/app/actions/categories";
 import BackButton from "@/components/backButton";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { db } from "@/db";
-import { categories } from "@/db/schema/categories";
-import { auth } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
 import { Link } from "next-view-transitions";
-import { redirect } from "next/navigation";
 
 export default async function CategoriesAll() {
-  const { userId } = auth();
-  if (!userId) redirect("/");
-
-  const userCategories = await db
-    .select()
-    .from(categories)
-    .where(eq(categories.userId, userId));
+  const categories = await getCategories();
 
   return (
     <div>
@@ -29,7 +19,7 @@ export default async function CategoriesAll() {
         <Link href="/dashboard/categories/new">Add New Category</Link>
       </Button>
       <div className="flex flex-col gap-1 mt-3">
-        {userCategories.map((cat) => (
+        {categories.map((cat) => (
           <div className="flex flex-col gap-1 pt-1">
             <div className="font-mono">{cat.name}</div>
             <div
