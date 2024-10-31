839e6d7504e86df596c0d01180979a4458aa4f31
Davide Lombardi <davidelombardi130p@gmail.com>
2024-10-31 19:25:04 +0100

    feat: sidebar for desktop view

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Navigation from "@/components/navigation";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default async function DashboadLayout({
   children,
@@ -6,11 +7,15 @@ export default async function DashboadLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative">
-      <Navigation />
-      <div className="md:w-5/6 lg:w-3/5 mx-auto mt-4 mb-[CALC(60px_+_env(safe-area-inset-bottom))] px-4">
-        {children}
-      </div>
+    <div className="bg-[rgba(39,39,42,1)]">
+      <SidebarProvider>
+        <Navigation />
+        <main className="bg-primary-foreground md:m-2 md:rounded-lg w-full">
+          <div className="md:w-5/6 lg:w-3/5 mx-auto mt-4 mb-[CALC(60px_+_env(safe-area-inset-bottom))] px-4">
+            {children}
+          </div>
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,10 +3,82 @@
 import { cn } from "@/lib/utils";
 import { Link } from "next-view-transitions";
 import { usePathname } from "next/navigation";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import { CirclePlus, Home, Repeat, Settings } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Navigation() {
+  const { isLoaded, userId } = useAuth();
+
   const currentPath = usePathname();
+  const isMobile = useIsMobile();
+
+  const items = [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: Home,
+    },
+    {
+      title: "New Transaction",
+      url: "/dashboard/transactions/new",
+      icon: CirclePlus,
+    },
+    {
+      title: "Recurring",
+      url: "/dashboard/recurring",
+      icon: Repeat,
+    },
+    {
+      title: "Settings",
+      url: "/dashboard/settings",
+      icon: Settings,
+    },
+  ];
+
+  if (!isLoaded || !userId) {
+    return null;
+  }
+
+  if (!isMobile)
+    return (
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel className="font-jura font-bold text-4xl -ml-1 mb-6 mt-4 text-background">
+              $
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      isActive={currentPath === item.url}
+                    >
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    );
 
   return (
     <div className="fixed z-10 bottom-0 left-0 flex justify-around w-full bg-primary-foreground md:bg-transparent">
